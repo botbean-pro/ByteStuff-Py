7706455f0c40257d10b8c15426e7b29258022b2a
BotBean-Pro <shriyankalra26@gmail.com>
2025-06-06 12:02:44 +0530

    stuff

---
+++ stuff.py
@@ -0,0 +1,5 @@
+MyFavTeam = str("Choose your favorite team in IPL - ")
+if MyFavTeam.lower() == "rcb" or "royal challengers bangalore":
+    print("You are a true RCB fan!")
+else:
+    print("u r dum")
